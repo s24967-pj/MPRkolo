a1d1f6d6031bdf51060216e06b0a7d53bb89cc65
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-01-21 12:12:57 +0100

    pierwszy commit

---
+++ kolokwium/kolokwium/src/main/java/com/example/kolokwium/KolokwiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.kolokwium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolokwiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KolokwiumApplication.class, args);
+	}
+
+}
---
+++ kolokwium/kolokwium/src/test/java/com/example/kolokwium/KolokwiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kolokwium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolokwiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
